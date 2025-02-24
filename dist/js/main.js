a8f34aafd25ee39c391c54425b858233cb2655fc
SerheyBr <sergeydrilkov@mail.ru>
2025-02-24 23:41:33 +0300

    finish proj

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,16 @@
 
 
+
+const swiper = new Swiper('.testimonial-slider', {
+   // Optional parameters
+   // direction: 'vertical',
+   slidesPerView: 3,
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.testimonial-slider__btn-right',
+      prevEl: '.testimonial-slider__btn-left',
+   },
+})
+
 //# sourceMappingURL=main.js.map
---
+++ src/js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.testimonial-slider', {
+   // Optional parameters
+   // direction: 'vertical',
+   slidesPerView: 3,
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.testimonial-slider__btn-right',
+      prevEl: '.testimonial-slider__btn-left',
+   },
+})
